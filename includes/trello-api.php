c6d306904c20fb781da4eec800395859e29d2a74
Christopher Cordine <gryphbecrazeh@gmail.com>
2020-04-03 13:02:41 -0400

    Added Composer files

--- includes/trello-api.php
+++ includes/trello-api.php
@@ -19,18 +19,19 @@ class TRELLO_API
         else
             return FALSE;
     }
-    public function GetBoard()
-    {
-        $curl = curl_init();
-        // here is the issue where $url isn't defined
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($curl);
-        $json = json_decode($response);
-        return $json;
-    }
+    // public function GetBoard()
+    // {
+    //     $curl = curl_init();
+    //     // here is the issue where $url isn't defined
+    //     curl_setopt($curl, CURLOPT_URL, $url);
+    //     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    //     $response = curl_exec($curl);
+    //     $json = json_decode($response);
+    //     return $json;
+    // }
     function RenderResults($boardId)
     {
+
         $plugin_url = ABSPATH . 'wp-content/plugins/eey-reporting';
 
         $str = file_get_contents($plugin_url . '/includes/config/Trello-API.json');
@@ -46,7 +47,6 @@ class TRELLO_API
 
         $url = "https://api.trello.com/1/boards/$boardId/?lists=all&cards=all&key={$key}&token={$token}";
 
-
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -55,9 +55,17 @@ class TRELLO_API
         if ($json) {
             $lists = $json->lists;
             $cards = $json->cards;
+            // function FilterList($item)
+            // {
+            //     if ($item->name === "Completed")
+            //         return TRUE;
+            //     else
+            //         return FALSE;
+            // }
 
+            // foreach (array_filter($lists, "FilterList") as $list) {
+            foreach (array_filter($lists, array($this, 'FilterList')) as $list) {
 
-            foreach (array_filter($lists, "FilterList") as $list) {
 ?>
                 <p>Completed tasks in <?php echo date('M') ?></p>
                 <ul>
@@ -74,8 +82,7 @@ class TRELLO_API
                 </ul>
 <?php
             }
-        }
-        else {
+        } else {
             echo 'Board Not Found...';
             echo '<br>';
             echo "Cannot find <a href='https://trello.com/b/$boardId'>https://trello.com/b/$boardId</a>...";
