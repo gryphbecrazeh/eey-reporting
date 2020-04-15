6623fc54a3f9f9633f65e03e5c06220037bddb18
Christopher Cordine <gryphbecrazeh@gmail.com>
2020-04-15 16:57:56 -0400

    notes and quick touch ups for tomorrow

--- includes/forms/generate_report.php
+++ includes/forms/generate_report.php
@@ -19,11 +19,23 @@ function generate_report($ID)
     include_once $plugin_url . '/includes/google-analytics.php';
 
     include_once $plugin_url . '/includes/forms/ga_inc/ga_report_class.php';
-
+    ?>
+    <form action="post"></form>
+    
+    
+    <?php
+    
     $trello = new TRELLO_API();
     $trello_report = $trello->getData($result->trello_board_id);
-    $trello->RenderResults($trello_report);
+    // Serialize the received report
+    $serialized_trello_report = serialize($trello_report);
+
+    // $unserialized = unserialize($serialized_trello_report);
+
+    // $trello->RenderResults($unserialized);
+
     die();
+
     $google = new GA_API();
     $google->report($result->ga_view_id);
 }
